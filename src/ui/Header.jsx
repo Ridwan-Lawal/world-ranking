a55a653c7a09be274fbe60168fc254d3e972b9c0
Ridwan-Lawal <lawal6382@gmail.com>
2024-05-11 14:04:48 +0100

    updated header

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 function Header() {
   const navigate = useNavigate();
   return (
-    <header className=" h-[37vh] md:h-[50vh] bg-heroImage bg-cover bg-no-repeat bg-center flex items-center justify-center">
+    <header className=" h-[28vh] md:h-[40vh] bg-heroImage bg-cover bg-no-repeat bg-center flex items-center justify-center">
       <div className="cursor-pointer" onClick={() => navigate("/")}>
         <img src="/assets/Logo.svg" alt="" className="w-48" />
       </div>
